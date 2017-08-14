343857ef8366b185679c3a1cc4e93b4fd207043f
José Roberto Colombo Junior <colombojrj@gmail.com>
2017-08-13 23:08:22 -0300

    Initial support of HAL_INIT

---
+++ HAL/HAL.cpp
@@ -0,0 +1,21 @@
+#include "HAL.h"
+#include "../config.h"
+
+void HAL_init()
+{
+    // Initialize the TIMERs
+    INIT_TIMER0A(0);
+    INIT_TIMER0B(0);
+    INIT_TIMER1A(0);
+    INIT_TIMER1B(0);
+    INIT_TIMER2A(0);
+    INIT_TIMER2B(0);
+
+    // Initialize the ADC
+
+
+    // Initialize the SPI
+
+}
+
+
